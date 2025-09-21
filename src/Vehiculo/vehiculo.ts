6bfbbb21e47df38dbfca3759ba72aff0a0dfae18
Gonzalo Garcia Madotti <gonzagmadotti@gmail.com>
2025-09-21 14:35:02 -0300

    npm i

---
+++ src/Vehiculo/vehiculo.ts
@@ -0,0 +1,13 @@
+import { estadoVehiculo } from "../Vehiculo/estadoVehiculo"
+
+export default abstract class Vehiculo {
+
+  private matricula: string;
+  private estadoVehiculo: ESTADO_VEHICULO;
+  
+  protected tarifaBase: number;
+  
+
+
+
+}
