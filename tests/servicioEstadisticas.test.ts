a693ce185eb65a316480f0b57401b6eb8320a1f7
Gonzalo Garcia Madotti <gonzagmadotti@gmail.com>
2025-11-21 15:53:10 -0300

    Test de la clase ServicioEstadisticas

--- tests/servicioEstadisticas.test.ts
+++ tests/servicioEstadisticas.test.ts
@@ -162,4 +162,89 @@ describe("Test de la clase ServicioEstadisticas", () => {
     );
   })
 
+  // PORCENTAJE DE OCUPACIÓN DE LA FLOTA
+  it("Debe calcular el porcentaje de ocupación de la flota de forma correcta", () => {
+    const vehiculos: Vehiculo[] = [vehiculoMock1, vehiculoMock2, vehiculoMock3];
+    const reservas: Reserva[] = [];
+    estadisticas = new ServicioEstadisticas(vehiculos, reservas);
+    expect(vehiculoMock1.getEstado().getNombre()).toBe("En Alquiler")
+    expect(vehiculoMock2.getEstado().getNombre()).toBe("Disponible")
+    expect(vehiculoMock3.getEstado().getNombre()).toBe("En Alquiler")
+    // 2 de 3 vehiculos están alquilados = 66.666..% => 66.67
+    expect(estadisticas.porcentajeDeOcupacionFlota()).toBeCloseTo(66.67, 1);
+  })
+
+  it("Debe devolver 0 el porcentaje de ocupación de la flota si no hay vehiculos", () => {
+    const vehiculos: Vehiculo[] = [];
+    const reservas: Reserva[] = [];
+    estadisticas = new ServicioEstadisticas(vehiculos, reservas);
+    expect(estadisticas.porcentajeDeOcupacionFlota()).toBe(0);
+  })
+
+  it("Debe devolver 0 también si no hay ningún vehiculo en alquiler", () => {
+    vehiculoMock1.getEstado = jest.fn().mockReturnValue(
+      { getNombre: jest.fn().mockReturnValue("Disponible") }
+    )
+    vehiculoMock3.getEstado = jest.fn().mockReturnValue(
+      { getNombre: jest.fn().mockReturnValue("Disponible") }
+    )
+    const vehiculos: Vehiculo[] = [vehiculoMock1, vehiculoMock2, vehiculoMock3];
+    const reservas: Reserva[] = [];
+    estadisticas = new ServicioEstadisticas(vehiculos, reservas);
+    expect(estadisticas.porcentajeDeOcupacionFlota()).toBe(0);
+  })
+
+  it("Debe devolver 100 si todos los vehiculos están en alquiler", () => {
+    vehiculoMock2.getEstado = jest.fn().mockReturnValue(
+      { getNombre: jest.fn().mockReturnValue("En Alquiler") }
+    )
+    const vehiculos: Vehiculo[] = [vehiculoMock1, vehiculoMock2, vehiculoMock3];
+    const reservas: Reserva[] = [];
+    estadisticas = new ServicioEstadisticas(vehiculos, reservas);
+    expect(estadisticas.porcentajeDeOcupacionFlota()).toBe(100);
+  })
+
+  // CALCULAR RENTABILIDAD
+  it("Debe calcular la rentabilidad de un vehículo de forma correcta", () => {
+    const vehiculos: Vehiculo[] = [vehiculoMock2];
+    const reservas: Reserva[] = [reservaMock2, reservaMock4];
+    estadisticas = new ServicioEstadisticas(vehiculos, reservas);
+    expect(estadisticas.calcularRentabilidad(vehiculoMock2)).toBe(4500);
+    // Ingresos: 5000 + 1000 = 6000
+    // Costos: 1000 + 500 = 1500
+    // Rentabilidad: 6000 - 1500 = 4500
+  })
+
+  it("Debe devolver una rentabilidad negativa si los costos superan los ingresos", () => {
+    vehiculoMock1.getMantenimientos = jest.fn().mockReturnValue([
+      { getCosto: jest.fn().mockReturnValue(5000) }
+    ])
+    const vehiculos: Vehiculo[] = [vehiculoMock1];
+    const reservas: Reserva[] = [reservaMock1];
+    estadisticas = new ServicioEstadisticas(vehiculos, reservas);
+    expect(estadisticas.calcularRentabilidad(vehiculoMock1)).toBe(-2000);
+    // Ingresos: 3000
+    // Costos: 5000
+    // Rentabilidad: -2000
+  })
+
+  it("Se debe ignorar reserva que no tenga kilometros recorridos", () => {
+    let reservaMock5: jest.Mocked<Reserva>;
+    reservaMock5 = {
+      getVehiculo: jest.fn().mockReturnValue(vehiculoMock1),
+      getFechaDeInicio: jest.fn().mockReturnValue(new Date(2025, 10, 4)),
+      getFechaDeFin: jest.fn().mockReturnValue(new Date(2025, 10, 8)),
+      getKilometrosRecorridos: jest.fn().mockReturnValue(0),
+      calcularCostoTotal: jest.fn().mockReturnValue(1000)     
+    } as unknown as jest.Mocked<Reserva>
+    const vehiculos: Vehiculo[] = [vehiculoMock1];
+    const reservas: Reserva[] = [reservaMock1, reservaMock5];
+    estadisticas = new ServicioEstadisticas(vehiculos, reservas);
+    expect(estadisticas.calcularRentabilidad(vehiculoMock1)).toBe(2300);
+    // Solo se cuenta la reservaMock3
+    // Ingresos: 3000
+    // Costos: 700
+    // Rentabilidad: 2300
+  })
+
 })
